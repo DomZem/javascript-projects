a42b149036a556b00900c406433170ddc44481c8
Dominik <dominik.zemlik.dev@gmail.com>
2024-04-13 21:31:55 +0200

    update styles for lab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,4 +1,4 @@
-const calculateBtn = document.querySelector('.calculate-btn');
+//const calculateBtn = document.querySelector('.calculate-btn');
 const addNewInputBtn = document.querySelector('.add-new-input-btn');
 
 // Output
@@ -7,55 +7,56 @@ const avgValue = document.querySelector('.avg-value');
 const minValue = document.querySelector('.min-value');
 const maxValue = document.querySelector('.max-value');
 
-const inputContainer = document.querySelector('.inputs-container');
+const inputContainer = document.querySelector('.input-list');
 
 const calculate = () => {
-    const inputs = document.querySelectorAll('.input');
-    const values = [];
+	const inputs = document.querySelectorAll('.input-list-item-input');
+	const values = [];
 
-    inputs.forEach((input) => {
-        values.push(parseInt(input.value) || 0);
-    });
+	inputs.forEach((input) => {
+		values.push(parseInt(input.value) || 0);
+	});
 
-    const sum = values.reduce((acc, value) => acc += value, 0);
+	const sum = values.reduce((acc, value) => (acc += value), 0);
 
-    sumValue.textContent = sum;
-    avgValue.textContent = Math.floor(sum / values.length);
-    minValue.textContent = Math.min(...values);
-    maxValue.textContent = Math.max(...values);
+	sumValue.textContent = sum;
+	avgValue.textContent = Math.floor(sum / values.length);
+	minValue.textContent = Math.min(...values);
+	maxValue.textContent = Math.max(...values);
 };
 
 addNewInputBtn.addEventListener('click', () => {
-    // create new list item 
-    const newListItem = document.createElement('li');
-    const newInput = document.createElement('input');
-    const newRemoveInputBtn = document.createElement('button');
-    newRemoveInputBtn.textContent = "x";
-    
-    newInput.className = "input";
-    newListItem.appendChild(newInput);
-    newListItem.appendChild(newRemoveInputBtn);
+	const newListItem = document.createElement('li');
+	newListItem.className = 'input-list-item';
+	const newInput = document.createElement('input');
+	newInput.type = 'number';
+	const newRemoveInputBtn = document.createElement('button');
+	newRemoveInputBtn.textContent = 'Remove';
+	newRemoveInputBtn.className = 'button button-destructive';
 
-    // add new input to list container
-    inputContainer.appendChild(newListItem);
+	newInput.className = 'input-list-item-input';
+	newListItem.appendChild(newInput);
+	newListItem.appendChild(newRemoveInputBtn);
+
+	inputContainer.appendChild(newListItem);
 });
 
-calculateBtn.addEventListener('click', () => {
-    calculate();
-});
+//calculateBtn.addEventListener('click', () => {
+//calculate();
+//});
 
 inputContainer.addEventListener('click', (event) => {
-    if (event.target.tagName === 'BUTTON') {
-        const listItem = event.target.parentElement;
-        inputContainer.removeChild(listItem);
-        
-        // after remove some input calculate results once again
-        calculate();
-    }
+	if (event.target.tagName === 'BUTTON') {
+		const listItem = event.target.parentElement;
+		inputContainer.removeChild(listItem);
+
+		// after remove some input calculate results once again
+		calculate();
+	}
 });
 
-inputContainer.addEventListener('change', (event) => {
-    if (event.target.classList.contains('input')) {
-        calculate();
-    }
-})
+inputContainer.addEventListener('input', (event) => {
+	if (event.target.classList.contains('input-list-item-input')) {
+		calculate();
+	}
+});
